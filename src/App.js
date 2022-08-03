7b59f4a595abd65a4bb9b5bc1ba6d5ff840f56dc
Magalitraversi <Traversiaylen@gmail.com>
2022-08-02 21:16:33 -0300

    componente detail

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from './componentes/Footer.jsx';
 import Navbar from './componentes/Navbar.jsx';
 import ItemListContainer from './componentes/ItemListContainer'
+import ItemDetailContainer from './componentes/ItemDetailContainer.jsx';
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Navbar />
       <h1 className='text-center'>Ecommerce Deliciosos mimos  </h1>
       <ItemListContainer />
-
+      <ItemDetailContainer />
       {/* <Footer /> */}
     </div>
   );
---
+++ src/Utils/ProductoMuestra.js
@@ -0,0 +1,14 @@
+const ProductoMuestra = [
+    {
+        id: 1,
+        titulo: "Producto DETAIL",
+        descripcion: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque quam magnam fuga, necessitatibus officia voluptas vel dolor veritatis dicta amet facilis commodi accusamus aut assumenda! Ut esse officia explicabo quos!",
+        imagen: "https://cuk-it.com/wp-content/uploads/2020/07/thumb02-1-1024x576.jpg",
+        stock: 5,
+        precio: 2500, 
+
+    }, 
+
+]
+
+export default ProductoMuestra;
